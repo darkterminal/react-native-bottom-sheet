7ad4c1d2258f1bd0589fe012e44129ae6ccd847c
Imam Ali Mustofa <d.darkterminal@gmail.com>
2022-08-12 22:14:10 +0700

    chore: initial commit for BottomSheet React Native

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useRef } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet from './components/BottomSheet';
 
 export default function App() {
+
+  const ref = useRef(null)
+  const onPressHandler = useCallback(() => {
+    const isActive = ref?.current?.isActive()
+    if(isActive) {
+      ref?.current.scrollTo(0)
+    } else {
+      ref?.current.scrollTo(-200)
+    }
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <TouchableOpacity style={styles.button} onPress={onPressHandler} />
+        <BottomSheet ref={ref}>
+          <View style={{ flex: 1, backgroundColor: 'orange' }} />
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    height: 50,
+    borderRadius: 25,
+    aspectRatio: 1,
+    backgroundColor: 'white',
+    opacity: 0.6
+  }
 });
